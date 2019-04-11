a2070872330c06b6fb06b82afbfee2339601ad3a
Joaquín Moreno Fernández <joaquin.moreno@grupoesfera.com.ar>
2019-04-11 18:47:48 -0300

    Crea app que conecta a dispositivo por bluetooth

---
+++ android/app/src/main/java/ar/edu/untref/trackeame_mobile/MainActivity.java
@@ -0,0 +1,13 @@
+package ar.edu.untref.trackeame_mobile;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,298 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Trackeame',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: BluetoothWidget(),
+    );
+  }
+}
+
+class BluetoothWidget extends StatefulWidget {
+
+  @override
+  _BluetoothWidgetState createState() => _BluetoothWidgetState();
+}
+
+class _BluetoothWidgetState extends State<BluetoothWidget>{
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  // Get the instance of the bluetooth
+  FlutterBluetoothSerial bluetooth = FlutterBluetoothSerial.instance;
+
+  // Define some variables, which will be required later
+  List<BluetoothDevice> _devicesList = [];
+  BluetoothDevice _device;
+  bool _connected = false;
+  bool _pressed = false;
+
+  @override
+  void initState() {
+    super.initState();
+    bluetoothConnectionState();
+  }
+
+  Future<void> bluetoothConnectionState() async {
+    List<BluetoothDevice> devices = [];
+
+    // To get the list of paired devices
+    try {
+      devices = await bluetooth.getBondedDevices();
+    } on PlatformException {
+      print("Error");
+    }
+
+    bluetooth.onStateChanged().listen((state) {
+      switch (state) {
+        case FlutterBluetoothSerial.CONNECTED:
+          setState(() {
+            _connected = true;
+            _pressed = false;
+          });
+
+          break;
+
+        case FlutterBluetoothSerial.DISCONNECTED:
+          setState(() {
+            _connected = false;
+            _pressed = false;
+          });
+          break;
+
+        default:
+          print(state);
+          break;
+      }
+    });
+
+    // It is an error to call [setState] unless [mounted] is true.
+    if (!mounted) {
+      return;
+    }
+
+    // Store the [devices] list in the [_devicesList] for accessing
+    // the list outside this class
+    setState(() {
+      _devicesList = devices;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          title: Text("Trackeame Bluetooth"),
+          backgroundColor: Colors.redAccent,
+        ),
+        body: Container(
+          // Defining a Column containing FOUR main Widgets wrapped with some padding:
+          // 1. Text
+          // 2. Row
+          // 3. Card
+          // 4. Text (wrapped with "Expanded" and "Padding")
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Text(
+                  "DISPOSITIVOS",
+                  style: TextStyle(fontSize: 24),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                // Defining a Row containing THREE main Widgets:
+                // 1. Text
+                // 2. DropdownButton
+                // 3. RaisedButton
+                child: Card(
+                  elevation: 4,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: <Widget>[
+                        Text(
+                            'Elegir:',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                        ),
+                        Expanded(
+                          child: DropdownButton(
+                            // To be implemented : _getDeviceItems()
+                            items: _getDeviceItems(),
+                            onChanged: (value) => setState(() => _device = value),
+                            value: _device,
+                          )
+                        ),
+                        RaisedButton(
+                          onPressed:
+                          // To be implemented : _disconnect and _connect
+                          _pressed ? null : _connected ? _disconnect : _connect,
+                          child: Text(_connected ? 'Desconectar' : 'Conectar'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Card(
+                  elevation: 4,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    // Defining a Row containing THREE main Widgets:
+                    // 1. Text (wrapped with "Expanded")
+                    // 2. FlatButton
+                    // 3. FlatButton
+                    child: Column(
+                      children: <Widget>[
+                          Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(
+                                "Dispositivo Vinculado",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.green,
+                                ),
+                              ),
+                            ),
+                            FlatButton(
+                              onPressed:
+                              // To be implemented : _sendOnMessageToBluetooth()
+                              _connected ? _sendOnMessageToBluetooth : null,
+                              child: Text("ON"),
+                            ),
+                            FlatButton(
+                              onPressed:
+                              // To be implemented : _sendOffMessageToBluetooth()
+                              _connected ? _sendOffMessageToBluetooth : null,
+                              child: Text("OFF"),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Center(
+                    child: Text(
+                      "NOTE: If you cannot find the device in the list, "
+                          "please turn on bluetooth and pair the device by "
+                          "going to the bluetooth settings",
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<DropdownMenuItem<BluetoothDevice>> _getDeviceItems() {
+    List<DropdownMenuItem<BluetoothDevice>> items = [];
+    if (_devicesList.isEmpty) {
+      items.add(DropdownMenuItem(
+        child: Text('NONE'),
+      ));
+    } else {
+      _devicesList.forEach((device) {
+        items.add(DropdownMenuItem(
+          child: Text(device.name),
+          value: device,
+        ));
+      });
+    }
+    return items;
+  }
+
+  // Method to connect to bluetooth
+  void _connect() {
+    if (_device == null) {
+      show('No hay dispositivo seleccionado');
+    } else {
+      bluetooth.isConnected.then((isConnected) {
+        if (!isConnected) {
+          bluetooth
+              .connect(_device)
+              .timeout(Duration(seconds: 10))
+              .catchError((error) {
+            setState(() => _pressed = false);
+          });
+          setState(() => _pressed = true);
+        }
+      });
+    }
+  }
+
+  // Method to disconnect bluetooth
+  void _disconnect() {
+    bluetooth.disconnect();
+    setState(() => _pressed = true);
+  }
+
+  // Method to show a Snackbar,
+  // taking message as the text
+  Future show(
+      String message, {
+        Duration duration: const Duration(seconds: 3),
+      }) async {
+    await new Future.delayed(new Duration(milliseconds: 100));
+    _scaffoldKey.currentState.showSnackBar(
+      new SnackBar(
+        content: new Text(
+          message,
+        ),
+        duration: duration,
+      ),
+    );
+  }
+
+  // Method to send message,
+  // for turning the bletooth device on
+  void _sendOnMessageToBluetooth() {
+    bluetooth.isConnected.then((isConnected) {
+      if (isConnected) {
+        bluetooth.write("1");
+        show('Device Turned On');
+      }
+    });
+  }
+
+  // Method to send message,
+  // for turning the bletooth device off
+  void _sendOffMessageToBluetooth() {
+    bluetooth.isConnected.then((isConnected) {
+      if (isConnected) {
+        bluetooth.write("0");
+        show('Device Turned Off');
+      }
+    });
+  }
+}
